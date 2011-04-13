4e0076701bfe743b467e7fd7db34d826bbfa87ab
Will Boyce <me@willboyce.com>
2011-04-13 12:31:52 +0100

    added install script

---
+++ tools/install.sh
@@ -0,0 +1,10 @@
+if [ -d ~/.vim ] || [ -f ~/.vimrc ] || [ -h ~/.vimrc]; then
+    echo "Previous vim config found at ~/.vim/ or ~/.vimrc! Bailing..." >&2
+    exit
+fi
+
+echo "Cloning repo..."
+/usr/bin/env git clone https://github.com/wrboyce/vimrc.git ~/.vim
+
+echo "Installing config..."
+ln -s ~/.vim/vimrc ~/.vimrc
