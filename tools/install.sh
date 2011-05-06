3db35b636f45e8737558f90725a3f2cd77a5478b
Will Boyce <me@willboyce.com>
2011-05-06 21:19:14 +0100

    Init submodules in tools/install.sh

--- tools/install.sh
+++ tools/install.sh
@@ -1,10 +1,11 @@
-if [ -d ~/.vim ] || [ -f ~/.vimrc ] || [ -h ~/.vimrc]; then
+if [ -d ~/.vim ] || [ -f ~/.vimrc ] || [ -h ~/.vimrc ]; then
     echo "Previous vim config found at ~/.vim/ or ~/.vimrc! Bailing..." >&2
     exit
 fi
 
 echo "Cloning repo..."
 /usr/bin/env git clone https://github.com/wrboyce/vimrc.git ~/.vim
+cd ~/.vim && git submodule update --init && cd -
 
 echo "Installing config..."
 ln -s ~/.vim/vimrc ~/.vimrc
